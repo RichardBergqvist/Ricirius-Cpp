fb50507e47f36389d573b29cb22d2e5b93faf2e1
Richard Bergqvist <richard.bergqvist2@gmail.com>
2017-04-11 00:10:32 +0200

    Improved Codebase

--- 3DEngineCpp/3DEngine.h
+++ 3DEngineCpp/3DEngine.h
@@ -3,13 +3,13 @@
 
 #include "../graphics/model.h"
 #include "../graphics/shader.h"
-#include "../core/transformer.h"
+#include "../util/transformer.h"
 #include "../components/camera.h"
 #include "../graphics/lighting.h"
 #include "../components/gameObject.h"
 #include "../graphics/modelRenderer.h"
 #include "../graphics/window.h"
 #include "../core/coreEngine.h"
-#include "../components/game.h"
+#include "../core/game.h"
 
 #endif // 3DENGINE_H_INCLUDED
